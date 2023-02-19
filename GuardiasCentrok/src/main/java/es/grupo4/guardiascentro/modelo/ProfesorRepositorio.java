d9b031fd232fb2a2d97f4bf9831d0160878acaf1
Lau23aldazabal <73839631+Lau23aldazabal@users.noreply.github.com>
2023-02-20 00:17:01 +0100

    Delete GuardiasCentrok directory

--- GuardiasCentrok/src/main/java/es/grupo4/guardiascentro/modelo/ProfesorRepositorio.java
+++
@@ -1,7 +0,0 @@
-package es.grupo4.guardiascentro.modelo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfesorRepositorio extends JpaRepository<Profesor,Integer>{
-
-}
