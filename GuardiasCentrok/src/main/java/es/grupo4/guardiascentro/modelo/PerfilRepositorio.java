f765ce87b77a4a31ec4d2008f69dc773784bf564
Lau23aldazabal <73839631+Lau23aldazabal@users.noreply.github.com>
2023-03-02 00:57:37 +0100

    Delete GuardiasCentrok directory

--- GuardiasCentrok/src/main/java/es/grupo4/guardiascentro/modelo/PerfilRepositorio.java
+++
@@ -1,7 +0,0 @@
-package es.grupo4.guardiascentro.modelo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerfilRepositorio extends JpaRepository<Perfil,Integer>{
-
-}
