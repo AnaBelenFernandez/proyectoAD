73fc54756eda6e216753ce511f24daa96b89e43c
@AnaFernandez <73839685+AnaBelenFernandez@users.noreply.github.com>
2023-03-03 14:51:27 +0100

    Delete GuardiasCentrok directory

--- GuardiasCentrok/src/main/java/es/grupo4/guardiascentro/modelo/HorarioRepositorio.java
+++
@@ -1,8 +0,0 @@
-package es.grupo4.guardiascentro.modelo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HorarioRepositorio extends JpaRepository<Horario, Integer>{
-	
-
-}
